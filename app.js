49a0c49764dc5caed5e36ccefcd46668fde22e70
jsunh98 <jsunh98@gmail.com>
2023-06-04 22:48:47 -0400

    added app.js, added css to make the letter transition work

---
+++ app.js
@@ -0,0 +1,4 @@
+let label = document.querySelectorAll('label').forEach(label => {
+    label.innerHTML = label.innerText.split('').map
+    ((letters, i) => `<span style="transition-delay: ${i * 50}ms">${letters}</span>`).join('');
+})
